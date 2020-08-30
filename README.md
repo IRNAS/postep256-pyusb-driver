6765a506d4612fa9217d3bf1913003ff3637f45d
musti <musti@wlan-si.net>
2020-08-30 13:08:11 +0200

    Operational version ready for testing.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # postep256-pyusb-driver
-Driver for PoLabs PoStep256
+Driver for PoLabs PoStep256 written in Python and using PyUSB package. See the `postep_test.py` file for usage example of the library.
+
+This library is under development and implementas basic functions. It assumes the driver is priperly configured though PoStep256 configuration application prior to use.
+
+Features available currently allow for the position control of the attached motor and to set it to operate at constant speed.
+
+## Installation Windows
+See https://github.com/pyusb/pyusb#installing
+
+## Installation Linux
+See https://github.com/pyusb/pyusb#installing
+
+
--- postep_test.py
+++ postep_test.py
@@ -1,232 +1,68 @@
-import os
-#os.environ['PYUSB_DEBUG'] = 'debug' # uncomment for verbose pyusb output
-import sys
-import platform
-import usb.core
-import usb.backend.libusb1
 import time
-import struct
+import logging
+import threading
+import sys
+from postep256usb import PoStep256USB
 
-VENDOR_ID = 0x1dc3
-PRODUCT_ID = 0x0641
-OUT_ENPOINT = 0x01
-IN_ENPOINT = 0x81
-
-def postep_enable_rt_stream(dev):
-
-    data_list = [0] * 64
-    # request data streaming
-    data_list[1] = 0xA0
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[0]!=0x02):
-        return False
-    return True
-
-def postep_read_stream(dev):
-    received = read_from_postep(dev, 500)
-    #parse data
-    #postep_stream["endswitch"]=received[6]
-    print(struct.unpack('>III', received[20:32]))
-
-def postep_run_sleep(dev,run):
-
-    data_list = [0] * 64
-    # request data streaming
-    data_list[1] = 0xA1
-    if run:
-        data_list[20] = 0x01
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[0]!=0x02):
-        return False
-    return True
-
-def postep_move_speed(dev,speed,direction="cw"):
-
-    data_list = [0] * 64
-    # request data streaming
-    data_list[1] = 0x90
-    #480000 kHz/step_value = speed
-    if speed is not 0:
-        step_values=480000/speed
-    else:
-        step_values=480000
-    data_list[20:24]=struct.pack('<I', int(step_values))
-    if direction == "acw":
-        data_list[24]=0x01
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[15]!=0x90):
-        return False
-    return True
-
-def postep_move_trajectory(dev,final_position,max_speed,max_accel,max_decel,direction,endsw=None):
-
-    data_list = [0] * 64
-    data_list[1] = 0xb1
-    #enable autorun
-    data_list[2]= 0x01<<1
-    # Set trajectory final position
-    data_list[20:23]=struct.pack('<I', final_position)
-    # Set trajectory max speed
-    data_list[24:27]=struct.pack('<I', max_speed)
-    # Set traject. max acceleration
-    data_list[28:31]=struct.pack('<I', max_accel)
-    # Set traject. max deceleration
-    data_list[32:35]=struct.pack('<I', max_decel)
-    # Set InvDir<<2|NCSw<<1| SwEn
-    if direction == "acw":
-        data_list[36]=0b00000100
-    if endsw is not None:
-        data_list[36]=data_list[36]|0b00000001
-        if endsw == "nc":
-            data_list[36]=data_list[36]|0b00000010
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[15]!=0xb1):
-        return False
-    return True
-
-def postep_stop_trajectory(dev):
-
-    data_list = [0] * 64
-    data_list[1] = 0xb2
+def wait_for_position():
     
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[0]!=0x02):
-        return False
-    return True
+    while True:
+        data = postep.read_stream()
+        #print("Received: {}".format(data))
+        if data["pos"] == data["final"]:
+            print("Reached: {}".format(data["final"]))
+            break
+        time.sleep(0.1)
 
-def postep_zero_trajectory(dev):
+postep = PoStep256USB(logging.INFO)
+# defined to show only errors as log values, set to logging.INFO or logging.DEBUG for more
 
-    data_list = [0] * 64
-    data_list[1] = 0xb3
-    
-    # write to driver
-    write_to_postep(dev,data_list)
-    # request data
-    received = read_from_postep(dev, 500)
-    # check if response is valid
-    if(received[0]!=0x02):
-        return False
-    return True
+# Check if driver was detected and configuration could be established
+if postep.device is None:
+    print("Driver not found, exiting.")
+    sys.exit(0)
 
-def postep_system_reset(dev):
+# enable streaming of real-time data
+postep.enable_rt_stream()
+# set the motor to run or sleep
+postep.run_sleep(True)
 
-    data_list = [0] * 64
-    data_list[1] = 0x02
-    
-    # write to driver
-    write_to_postep(dev,data_list)
+# Example 1: Run motor with fixed speed
+postep.move_speed(1000,"cw")
+time.sleep(2)
 
-def write_to_postep(dev,data_list):
+# Example 2: Use motor position control
 
-    #data_list = [0] * 64
-    #for run/sleep send data[1] = 0xA1
-    #data_list[0] = 0x01
-    #data_list[1] = 0x90
+# First reset the position to known value (do not do this if you wish the motor to retain position during program restart). Power cycle resets this also.
+postep.move_reset_to_zero()
+#configure speed acceleration and deceleration and endswitch for move commands
+postep.move_config(10000,1000,1000,None)
 
-    data = bytearray(data_list)
-    print("Writing command: {}".format(bytes(data).hex()))
+# Assuming there is a finite motion range defined, the end of range must be found first and configured to zero
+# Move slowly for fixed time to reach the end of range.
+postep.move_speed(100,"acw")
+time.sleep(5)
 
-    num_bytes_written = 0
-    try:
-        num_bytes_written = dev.write(OUT_ENPOINT, data,500)
-    except usb.core.USBError as e:
-        print (e.args)
+# Now define this as zero position
+postep.move_reset_to_zero()
 
-    return num_bytes_written
+# Move to a fixed position and wait
+postep.move_to(10)
+wait_for_position()
 
-def read_from_postep(dev, timeout):
-    try:
-        data = dev.read(IN_ENPOINT, 64, timeout)
-    except usb.core.USBError as e:
-        print ("Error reading response: {}".format(e.args))
-        return None
-    print("Receive command: {}".format(bytes(data).hex()))
-    if len(data) == 0:
-        return None
+postep.move_to(100)
+wait_for_position()
 
-    return data
+postep.move_to(1000)
+wait_for_position()
+
+postep.move_to(0)
+wait_for_position()
+postep.move_speed(100,"acw")
 
 
-was_kernel_driver_active = False
-device = None
-
-if platform.system() == 'Windows':
-    # required for Windows only
-    # libusb DLLs from: https://sourcefore.net/projects/libusb/
-    arch = platform.architecture()
-    if arch[0] == '32bit':
-        backend = usb.backend.libusb1.get_backend(find_library=lambda x: "libusb/x86/libusb-1.0.dll") # 32-bit DLL, select the appropriate one based on your Python installation
-    elif arch[0] == '64bit':
-        backend = usb.backend.libusb1.get_backend(find_library=lambda x: "libusb/x64/libusb-1.0.dll") # 64-bit DLL
-
-    device = usb.core.find(backend=backend, idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
-elif platform.system() == 'Linux':
-    device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
-
-    # if the OS kernel already claimed the device
-    if device.is_kernel_driver_active(0) is True:
-        # tell the kernel to detach
-        device.detach_kernel_driver(0)
-        was_kernel_driver_active = True
-else:
-    device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
-
-if device is None:
-    raise ValueError('ADU Device not found. Please ensure it is connected to the tablet.')
-    sys.exit(1)
-
-device.reset()
-
-# Set the active configuration. With no arguments, the first configuration will be the active one
-device.set_configuration()
-
-# Claim interface 0
-usb.util.claim_interface(device, 0)
-
-# Write
-#bytes_written=write_to_postep(device)
-#print("bytes_written: {}".format(bytes_written))
-
-##postep_system_reset(device)
-postep_enable_rt_stream(device)
-postep_run_sleep(device,True)
-#postep_move_speed(device,100,"cw")
-#postep_stop_trajectory(device)
-#time.sleep(1)
-#postep_move_speed(device,0,"acw")
-postep_move_trajectory(device,10000,10000,10000,10000,"cw")
 while True:
-    postep_read_stream(device)
+    data = postep.read_stream()
+    if data != None:
+        print("Received: {}".format(data))
     time.sleep(1)
-
-
-# Read
-data = read_from_postep(device, 500) # read from device with a 200 millisecond timeout
-if data != None:
-    print("Received string: {}".format(bytes(data).hex()))
-
-usb.util.release_interface(device, 0)
-
-# This applies to Linux only - reattach the kernel driver if we previously detached it
-if was_kernel_driver_active == True:
-    device.attach_kernel_driver(0)
